350c8d35c63527c131da27ec6934ecc6b24878fe
Olena Oleksandrova <ha240886kev@gmail.com>
2024-11-29 23:36:34 +0200

    css

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -8,7 +8,7 @@ const SearchBox = () => {
 
   return (
     <div className={s.inputForm}>
-      <p className={s.textFind}>Find contacts by name</p>
+      <p className={s.textFind}>Find contacts by name: </p>
       <input
         className={s.input}
         type="text"
